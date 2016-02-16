08508b45af06334644713349c7169af30ad6ad04
Jason S. Jones <jsjones96@gmail.com>
2016-02-15 21:03:05 -0800

    Use user controller to post user to db

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -9,12 +9,5 @@ module.exports = function (api) {
         // .get(authCtrl.isAuthenticated, userCtrl.getUsers);
 
     api.route('/register')
-        .post(function (req, res) {
-            console.log(req.body);
-            res.json({
-                success: true,
-                message: 'New coffee drinker added to the coffee messs',
-                name: req.body.username
-            });
-        });
+        .post(userCtrl.postUsers);
 };
