f39af624a1bfcc781b2fca1d579080ac89fbb06f
Jason S. Jones <jsjones96@gmail.com>
2016-03-16 20:12:36 -0700

    Remove passport-http

--- api/routes/coffee.routes.js
+++ api/routes/coffee.routes.js
@@ -1,15 +1,13 @@
 var coffeeCtrl = require('../controllers/coffee.controller');
-var authCtrl = require('../controllers/auth.controller');
 
 module.exports = function (api) {
 
     // Create new route with prefix /coffee
     api.route('/coffee')
-        .post(authCtrl.isAuthenticated, coffeeCtrl.postCoffees)
         .get(coffeeCtrl.getCoffees);
 
     api.route('/coffee/:coffeeId')
-        .get(authCtrl.isAuthenticated, coffeeCtrl.getCoffee)
-        .put(authCtrl.isAuthenticated, coffeeCtrl.putCoffee)
-        .delete(authCtrl.isAuthenticated, coffeeCtrl.deleteCoffee);
+        .get(coffeeCtrl.getCoffee)
+        .put(coffeeCtrl.putCoffee)
+        .delete(coffeeCtrl.deleteCoffee);
 };
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,12 +1,10 @@
 var userCtrl = require('../controllers/user.controller');
-// var authCtrl = require('../controllers/auth.controller');
 
 module.exports = function (api) {
 
     // Create new route with prefix /users
     api.route('/users')
         .post(userCtrl.postUsers);
-        // .get(authCtrl.isAuthenticated, userCtrl.getUsers);
 
     api.route('/register')
         .post(userCtrl.postUsers);
