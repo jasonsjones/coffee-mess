0cfb198bb0480d0384116e4c64235c24875ae77b
Jason Jones <jsjones96@gmail.com>
2016-04-17 14:46:05 +0000

    Setup google auth code request -- wip

--- public/app/core/auth.js
+++ public/app/core/auth.js
@@ -42,14 +42,24 @@
             return deferred.promise;
         }
 
+
         function googleAuth() {
             console.log('firing googleAuth in the auth service');
-            var url = 'https://accounts.google.com/o/oauth2/auth';
+            var deferred = $q.defer();
+
+            var queryBuilder = [];
+            queryBuilder.push('response_type=code',
+                            'client_id=334060514002-4f8a59tvvntacl6vb21oa762o4h9s89p.apps.googleusercontent.com',
+                            'redirect_uri=' + $window.location.origin +'/oauth/callback',
+                            'scope=profile email');
+
+            var url = 'https://accounts.google.com/o/oauth2/auth?' + queryBuilder.join('&');
 
             var options = 'width=500, height=500, left=' + ($window.outerWidth - 500) / 2 +
                           ', top =' + ($window.outerHeight - 500) / 2.5;
 
             $window.open(url, '', options);
+            return deferred.promise;
         }
     }
 }());
