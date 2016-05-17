2162945639960a74c785c3cfd349dbeea6395d1a
Jason Jones <jsjones96@gmail.com>
2016-05-17 14:38:05 +0000

    Create basic template for html email

--- api/app.js
+++ api/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var config = require('./config');
+var emailVerification = require('./services/emailVerification');
+
+emailVerification.send('fake@fake.com');
 
 // Connect to the coffeelocker MongoDB
 mongoose.connect(config.db);
--- api/services/emailVerification.js
+++ api/services/emailVerification.js
@@ -1,5 +1,14 @@
 var jwt = require('jwt-simple');
-var config = require('../../coffee-mess-cred.json');
+var config = require('../../coffee-mess-creds.json');
+var fs = require('fs');
+var _ = require('underscore');
+
+var model = {
+    verifyUrl: 'https://coffee-mess-jsj0nes.c9users.io/auth/verifyEmail?token=',
+    title: 'Coffee Mess',
+    subTitle: 'Thanks for signing up',
+    body: 'Please verify your email address by clicking the button below'
+};
 
 exports.send = function (email) {
     var payload = {
@@ -8,4 +17,20 @@ exports.send = function (email) {
 
     var token = jwt.encode(payload, config.emailSecret);
 
+    console.log(getHtml(token));
+};
+
+function getHtml(token) {
+    var path = __dirname + '/../views/emailVerification.html';
+    var html = fs.readFileSync(path, 'utf8');
+
+    var template = _.template(html);
+
+    model.verifyUrl += token;
+
+    return template(model);
+}
+
+_.templateSettings = {
+    interpolate: /\{\{(.+?)\}\}/g
 };
