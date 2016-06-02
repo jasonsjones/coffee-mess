0dd97f916cd46a6e31a35d2c4a14c6423b685cbf
Jason Jones <jsjones96@gmail.com>
2016-06-02 13:31:22 +0000

    Add grind method in the coffee model

--- api/models/coffee.js
+++ api/models/coffee.js
@@ -9,5 +9,12 @@ var CoffeeSchema = new mongoose.Schema({
     weightGround: Number
 });
 
+CoffeeSchema.methods.grind = function (amount) {
+    if (this.weightWholeBean > amount) {
+        this.weightWholeBean -= amount;
+        this.weightGround += amount;
+    }
+};
+
 // export the mongoose model
 module.exports = mongoose.model('Coffee', CoffeeSchema);
